34b001121d47ca62e5fae24919a6efbed1625280
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-15 01:33:13 -0400

    delete unecessary files

--- notifications_handler_class.py
+++
@@ -1,7 +0,0 @@
-from twilio.rest import Client
-class NotificationsHandler:
-    def __init__(self, account_sid, auth_token):
-        self.client = Client(account_sid, auth_token)
-    def send_text_message(self, to, from_, body):
-        text = self.client.messages.create(to=to, from_=from_, body=body)
-        return text
