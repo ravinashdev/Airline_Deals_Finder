45945d852a52ef9aa8dbaf7fd20260733c7385f0
Ryan Ramdehol <ravinashwork@gmail.com>
2026-04-15 01:12:52 -0400

    logic is too messy going to revamp it with a production style directory structure for API calls and the classes will be the API's themselves

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 # ---------------------------- IMPORTS ------------------------------- #
 import asyncio
+from config import settings
 from api_class import API
 
 
 # ---------------------------- CONSTANTS ------------------------------- #
-
+SHEETY_BEARER_TOKEN = settings.sheety.SHEETY_BEARER_TOKEN
+SERP_API_KEY = settings.serp.SERP_API_KEY
+TWILLIO_TEST_ACCOUNT_SID = settings.twillio.TWILLIO_TEST_ACCOUNT_SID
+TWILLIO_TEST_AUTH_TOKEN = settings.twillio.TWILLIO_TEST_AUTH_TOKEN
 # ---------------------------- GLOBAL VARIABLES ------------------------------- #
 
 # ---------------------------- FUNCTIONS ------------------------------- #
@@ -14,4 +18,8 @@ async def main():
         print(data)
 # ---------------------------- UI SETUP ------------------------------- #
 # Run all API calls
-asyncio.run(main())
+# asyncio.run(main())
+print(SHEETY_BEARER_TOKEN)
+print(SERP_API_KEY)
+print(TWILLIO_TEST_ACCOUNT_SID)
+print(TWILLIO_TEST_AUTH_TOKEN)
